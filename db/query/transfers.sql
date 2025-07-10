d10a710df8afee62de0cade3cd25c9027012b265
arbeeorlar <biola.gold@gmail.com>
2025-07-10 12:26:28 +0100

    unit testing the schema v2

---
+++ db/query/transfers.sql
@@ -0,0 +1,32 @@
+
+-- name: CreateTransfer :one
+INSERT INTO transfers (
+    from_account_id, to_account_id,amount
+) VALUES (
+             $1, $2,$3
+         )
+    RETURNING *;
+
+-- name: GetTransfers :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: GetTransferByFromAccountId :many
+SELECT * FROM transfers
+WHERE from_account_id=$1
+ORDER BY id ;
+
+-- name: GetTransferByToAccountId :many
+SELECT * FROM transfers
+WHERE to_account_id=$1
+ORDER BY id ;
+
+-- name: UpdateAccount :one
+-- UPDATE entries
+-- set owner = $2
+-- WHERE id = $1
+--     RETURNING *;
+--
+-- -- name: DeleteAccount :exec
+-- DELETE FROM account
+-- WHERE id = $1;
